402cf76f1d55bef9219eca38c05f200be5a41f5e
mstjn <matjuve33@icloud.com>
2026-05-04 11:41:28 +0200

    fix: remove unused states in create-article-form

--- components/form/create-article-form.tsx
+++ components/form/create-article-form.tsx
@@ -2,12 +2,10 @@
 
 import { createPost } from "@/lib/actions/articles";
 import { Topic } from "@/types";
-import { useActionState, useState } from "react";
+import { useActionState } from "react";
 
 export default function CreateArticleForm({ topics }: { topics: Topic[] }) {
   const [state, action] = useActionState(createPost, undefined);
-  const [title, setTitle] = useState("");
-  const [content, setContent] = useState("");
 
   return (
     <form action={action} className="flex flex-col gap-6 max-w-xl mx-auto w-full">
@@ -21,13 +19,13 @@ export default function CreateArticleForm({ topics }: { topics: Topic[] }) {
 
       <div className="flex flex-col gap-1">
         <label htmlFor="title" className="font-semibold">Titre</label>
-        <input id="title" name="title" type="text" value={title} onChange={e => setTitle(e.target.value)} className="border border-(--main-purple) rounded-lg h-12 px-3" />
+        <input id="title" name="title" type="text" className="border border-(--main-purple) rounded-lg h-12 px-3" />
         {state?.errors?.title && <p className="text-red-500 text-sm">{state.errors.title[0]}</p>}
       </div>
 
       <div className="flex flex-col gap-1">
         <label htmlFor="content" className="font-semibold">Contenu</label>
-        <textarea id="content" name="content" rows={8} value={content} onChange={e => setContent(e.target.value)} className="border border-(--main-purple) rounded-lg px-3 py-2 resize-none" />
+        <textarea id="content" name="content" rows={8} className="border border-(--main-purple) rounded-lg px-3 py-2 resize-none" />
         {state?.errors?.content && <p className="text-red-500 text-sm">{state.errors.content[0]}</p>}
       </div>
 
